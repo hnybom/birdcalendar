aafe8dc6eeac9426479937032b992592e966ef8e
Henri Nybom <henri.nybom@solita.fi>
2018-11-23 13:19:59 +0200

    fb rem

--- ui-app/src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyBkmjAJ5I_s1tbPYibXy2cc7TOvjdL1aGw',
-  authDomain: 'birdcalendar-hn.firebaseapp.com',
-  databaseURL: 'https://birdcalendar-hn.firebaseio.com',
-  projectId: 'birdcalendar-hn',
-  storageBucket: 'birdcalendar-hn.appspot.com',
-  messagingSenderId: '603078411546',
-};
